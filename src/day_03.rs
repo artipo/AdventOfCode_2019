642789fdb9188924760d9c9d58fdc3bbd80257d3
artipo <fede9421@hotmail.it>
2021-01-17 10:08:13 +0100

    better format

--- src/day_03.rs
+++ src/day_03.rs
@@ -14,14 +14,15 @@ pub fn solve(input: &str) -> String {
 fn puzzle_a(input: &str) -> String {
     let input_text = fs::read_to_string(input).unwrap();
 
-    let points_by_wire = input_text.trim()
+    let points_by_wire = input_text
+        .trim()
         .lines()
-        .map(|line| line.split(',')
-            .collect::<Vec<&str>>()) // line -> commands
+        .map(|line| line.split(',').collect::<Vec<&str>>()) // line -> commands
         .map(|command: Vec<&str>| -> HashSet<(i32, i32)> {
             let mut current_point = (0, 0);
-            
-            let points = command.into_iter()
+
+            let points = command
+                .into_iter()
                 .map(|step: &str| -> Vec<(i32, i32)> {
                     let (dir, n_text) = step.split_at(1);
                     let n = n_text.parse().unwrap();
@@ -32,30 +33,32 @@ fn puzzle_a(input: &str) -> String {
                         "D" => [(0, -1)].repeat(n),
                         _ => panic!(),
                     };
-    
+
                     steps
                 }) // command -> steps
                 .map(|steps| {
-                    steps.into_iter().map(|step| {
-                        current_point = (current_point.0 + step.0, current_point.1 + step.1);
-                        current_point
-                    })
-                    .collect::<Vec<(i32, i32)>>()
+                    steps
+                        .into_iter()
+                        .map(|step| {
+                            current_point = (current_point.0 + step.0, current_point.1 + step.1);
+                            current_point
+                        })
+                        .collect::<Vec<(i32, i32)>>()
                 }) // step -> points
                 .flatten()
                 .unique()
                 .collect::<HashSet<_>>();
-            
-            
+
             points
         }) // commands -> points
         .collect::<Vec<HashSet<(i32, i32)>>>();
 
     let first_wire = &points_by_wire[0];
     let second_wire = &points_by_wire[1];
-    let result = first_wire.intersection(second_wire)
-       .min_by_key(|point| point.0.abs() + point.1.abs())
-       .unwrap();
+    let result = first_wire
+        .intersection(second_wire)
+        .min_by_key(|point| point.0.abs() + point.1.abs())
+        .unwrap();
 
     let result_text = format!("{}", result.0 + result.1);
     result_text
@@ -64,14 +67,15 @@ fn puzzle_a(input: &str) -> String {
 fn puzzle_b(input: &str) -> String {
     let input_text = fs::read_to_string(input).unwrap();
 
-    let points_by_wire = input_text.trim()
+    let points_by_wire = input_text
+        .trim()
         .lines()
-        .map(|line| line.split(',')
-            .collect::<Vec<&str>>()) // line -> commands
+        .map(|line| line.split(',').collect::<Vec<&str>>()) // line -> commands
         .map(|command: Vec<&str>| -> HashSet<(i32, i32, i32)> {
             let mut current_point = (0, 0, 0);
-            
-            let points = command.into_iter()
+
+            let points = command
+                .into_iter()
                 .map(|step: &str| -> Vec<(i32, i32)> {
                     let (dir, n_text) = step.split_at(1);
                     let n = n_text.parse().unwrap();
@@ -82,15 +86,21 @@ fn puzzle_b(input: &str) -> String {
                         "D" => [(0, -1)].repeat(n),
                         _ => panic!(),
                     };
-    
+
                     steps
                 }) // command -> steps
                 .map(|steps| {
-                    steps.into_iter().map(|step| {
-                        current_point = (current_point.0 + step.0, current_point.1 + step.1, current_point.2 + 1);
-                        current_point
-                    })
-                    .collect::<Vec<(i32, i32, i32)>>()
+                    steps
+                        .into_iter()
+                        .map(|step| {
+                            current_point = (
+                                current_point.0 + step.0,
+                                current_point.1 + step.1,
+                                current_point.2 + 1,
+                            );
+                            current_point
+                        })
+                        .collect::<Vec<(i32, i32, i32)>>()
                 }) // step -> points
                 .flatten()
                 .unique_by(|point| (point.0, point.1))
@@ -100,27 +110,38 @@ fn puzzle_b(input: &str) -> String {
         }) // commands -> points
         .collect::<Vec<HashSet<(i32, i32, i32)>>>();
 
-    let first_wire = &points_by_wire[0].clone().into_iter().map(|point| (point.0, point.1)).collect::<HashSet<_>>();
+    let first_wire = &points_by_wire[0]
+        .clone()
+        .into_iter()
+        .map(|point| (point.0, point.1))
+        .collect::<HashSet<_>>();
     let first_wire_repo = &points_by_wire[0].clone();
-    let second_wire = &points_by_wire[1].clone().into_iter().map(|point| (point.0, point.1)).collect::<HashSet<_>>();
+    let second_wire = &points_by_wire[1]
+        .clone()
+        .into_iter()
+        .map(|point| (point.0, point.1))
+        .collect::<HashSet<_>>();
     let second_wire_repo = &points_by_wire[1].clone();
-    let result = first_wire.intersection(second_wire)
+    let result = first_wire
+        .intersection(second_wire)
         .map(|point| {
-            let first = first_wire_repo.into_iter()
+            let first = first_wire_repo
+                .into_iter()
                 .filter(|repo_point| (*point).eq(&(repo_point.0, repo_point.1)))
                 .nth(0)
                 .unwrap();
-            let second = second_wire_repo.into_iter()
+            let second = second_wire_repo
+                .into_iter()
                 .filter(|repo_point| (*point).eq(&(repo_point.0, repo_point.1)))
                 .nth(0)
                 .unwrap();
-            
+
             let sum_of_steps = first.2 + second.2;
             sum_of_steps
         })
         .min()
         .unwrap();
 
-        let result_text = format!("{}", result);
-        result_text
+    let result_text = format!("{}", result);
+    result_text
 }
